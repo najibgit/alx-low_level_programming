85df185814a5c8b1855f5f6c28dd6d7056031180
najibgit <najib.zakkabi@gmail.com>
2023-06-19 21:47:51 +0100

    1last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/** main - entry
+/** 
+ * main - entry level
  *
  * more headers goes there
  *
@@ -12,24 +12,23 @@
  */
 int main(void)
 {
-        int n;
-        int last_digit;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        last_digit = n % 10;
-        if (last_digit > 5)
-        {
-        printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-        }
-        else if ((last_digit < 6) && (last_digit != 0))
-        {
-                printf("Last digit of %d is %d is less than 6 and not 0\n", n, last_digit);
-        }
-        else if (last_digit == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, last_digit);
-        }
-        return (0);
+	int n;
+	int last_digit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last_digit = n % 10;
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if ((last_digit < 6) && (last_digit != 0))
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	return (0);
 }
