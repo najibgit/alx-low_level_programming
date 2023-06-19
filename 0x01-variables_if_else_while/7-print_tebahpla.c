155db0b985a1bb3618502d9ce897f2c9c34733a4
najibgit <najib.zakkabi@gmail.com>
2023-06-19 22:42:55 +0100

    6numbers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - lowercase alphabet in reverse
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
