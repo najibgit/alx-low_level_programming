c22b0469220bfba560274e9026ebf5f1569d543e
najibgit <najib.zakkabi@gmail.com>
2023-06-19 20:27:54 +0100

    posORneg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/**
+ * more headers goes there
+ * main log
+ * betty style doc for function main goes there
+ */
 
-/* betty style doc for function main goes there */
 int main(void)     /* main log */
 {
 	int n;
